96890e8e0456b44400ddea5bb6161713bb6da313
woomin <dal96k@gmail.com>
2020-09-15 17:59:13 +0900

    Fix session problem about session config
    처음에 express-session config의 resave 옵션을 true로 주었더니 세션이
    다시 저장되어 기존의 세션을 받아올 수 없는 문제가 있었습니다. 이 경우
    세션은 쿠키에 저장되어있긴 하지만 다시 받아올 수 없어서, resave를
    false로 주었고, 이제 새로고침하여도 잘 작동합니다.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors({ origin: "*", credentials: true }));
 app.use(session({
   secret: process.env.SESSION_SECRET,
-  resave: true,
+  resave: false,
   saveUninitialized: false,
   cookie: {},
 }));
