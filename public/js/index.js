d8b9c454b47df9192cd43ce8329c38d7d1826f32
woomin <dal96k@gmail.com>
2020-09-15 19:30:32 +0900

    Add simple todo frontend
    투두 기능을 테스트해보기 위한 간단한 프론트엔드 화면을 구현하였습니다.

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 const $app = document.getElementById('app');
-
 $app.innerHTML = `
   <input id='id_input' type='text' />
   <input id='password_input' type='text' />
   <button id='login_button' type='button'>로그인</button>
+  <br/>
+  <input id='todo_input' type='text' />
+  <button id='todo_button' type='button'>입력</button>
 `;
 
 const $login_button = document.getElementById('login_button');
@@ -16,9 +18,26 @@ $login_button.addEventListener('click', async () => {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ id, password }),
   });
-  const data = await response.json();
+});
 
-  const $user = document.getElementById('user');
+const $todo_button = document.getElementById('todo_button');
+$todo_button.addEventListener('click', async () => {
+  const content = document.getElementById('todo_input').value;
 
-  $user.innerHTML = `${data.message}`;
+  const response = await fetch('http://localhost:3000/todo', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ content }),
+  });
+
+  const $contents = document.getElementById('contents');
+
+  const data = await fetch('http://localhost:3000/todo');
+  const todos = await data.json();
+
+  $contents.innerHTML = `
+    <div id='todos'>
+      ${todos.map(todo => `<div>${todo.content}</div>`).join('')}
+    </div>
+  `;
 });
