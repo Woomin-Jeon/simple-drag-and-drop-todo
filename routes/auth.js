7e1c676fd1343b17ffe74bff12fc0ea98f804ec5
woomin <dal96k@gmail.com>
2020-09-15 12:34:38 +0900

    Add signup router
    회원가입하는 유저의 리퀘스트를 받아 처리하는 라우트를 만들었습니다.

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const { checkExistingUserById, addNewUser } = require('../database/auth');
 
+router.post('/signup', async (req, res) => {
+  const { id, password } = req.body;
+
+  const validation = await checkExistingUserById(id);
+
+  if (validation) {
+    res.status(400).send('Already existing id');
+    return;
+  }
+
+  await addNewUser(id, password);
+  res.status(200).send('Complete signup');
 });
+
+module.exports = router;
