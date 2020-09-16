2751027c79ec23f3c64857a1c25f44b070f78c4f
woomin <dal96k@gmail.com>
2020-09-16 20:19:14 +0900

    Add todo delete button
    투두 항목 옆에 삭제 버튼이 나오도록 만들었습니다.

--- public/src/components/TodoResult.js
+++ public/src/components/TodoResult.js
@@ -5,12 +5,25 @@ function TodoResult() {
   this.node = document.createElement('div');
   this.render = async () => {
     const todos = await axios.get('/todo');
-    
     this.node.innerHTML = `
       <div id='todo_result'>
-        ${todos.map(todo => `<div>${todo.content}</div>`).join('')}
+        ${todos.map(todo => `
+          <div>
+            <span>(${todo.category})</span>
+            <span>${todo.content}</span>
+            <button id=${todo.todoid} class='todo_delete_button'>삭제</button>
+          </div>`).join('')}
       </div>
     `;
+    const deleteButtons = document.querySelectorAll('#todo_result > div > .todo_delete_button');
+    deleteButtons.forEach(button => {
+      button.addEventListener('click', async (event) => {
+        const todoId = event.target.id;
+
+        await axios.post('/todo/delete', { todoId });
+        updator.forEach(func => func());
+      });
+    });
   };
 
   updator.push(this.render);
