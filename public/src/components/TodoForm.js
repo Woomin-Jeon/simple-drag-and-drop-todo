92e8662628b28849125eae15b210bf5fb3740d53
woomin <dal96k@gmail.com>
2020-09-16 16:52:45 +0900

    Modify file name and add event logic
    컴포넌트의 이름을 변경하고 이벤트 함수를 설정해주었습니다.

---
+++ public/src/components/TodoForm.js
@@ -0,0 +1,24 @@
+import Input from './Input.js';
+import Button from './Button.js';
+
+function TodoForm() {
+  this.node = document.createElement('div');
+  this.create = () => {
+    this.node.appendChild(Input({ id: 'todo_input', placeholder: '할 일을 입력해주세요'}));
+    this.node.appendChild(Button({ id: 'todo_button', title: '입력' }));
+  };
+
+  this.node.addEventListener('click', async () => {
+    const content = document.getElementById('todo_input').value;
+
+    const response = await fetch('http://localhost:3000/todo', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ content }),
+    });
+  });
+
+  this.create();
+}
+
+export default () => new TodoForm().node;
