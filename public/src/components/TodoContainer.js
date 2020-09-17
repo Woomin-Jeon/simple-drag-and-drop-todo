dbd626c96ee0547105e9a9fcf4000cf408a70bd2
woomin <dal96k@gmail.com>
2020-09-17 18:34:47 +0900

    Make todo result simple layout
    간단한 레이아웃을 적용하였습니다.

--- public/src/components/TodoContainer.js
+++ public/src/components/TodoContainer.js
@@ -2,8 +2,12 @@ import TodoResult from './TodoResult.js';
 
 function TodoContainer() {
   this.node = document.createElement('div');
+  this.node.classList.add('flex');
+
   this.render = () => {
-    this.node.appendChild(TodoResult());
+    this.node.appendChild(TodoResult({ category: 'todo' }));
+    this.node.appendChild(TodoResult({ category: 'doing' }));
+    this.node.appendChild(TodoResult({ category: 'done' }));
   }
 
   this.render();
--- public/src/components/TodoResult.js
+++ public/src/components/TodoResult.js
@@ -1,8 +1,9 @@
 import { updator, store, updateRendering } from '../store.js';
 import { deleteTodo } from '../apis/todo.js';
 
-function TodoResult() {
+function TodoResult({ category }) {
   this.node = document.createElement('div');
+  this.node.classList.add('todo_result');
 
   this.deleteButtonEvent = () => {
     const todoResult = document.querySelector('#todo_result');
@@ -20,9 +21,8 @@ function TodoResult() {
   this.render = async () => {
     this.node.innerHTML = `
       <div id='todo_result'>
-        ${store.todos.map(todo => `
+        ${store.todos.filter(todo => todo.category === category).map(todo => `
           <div>
-            <span>(${todo.category})</span>
             <span>${todo.content}</span>
             <button id=${todo.todoid} class='todo_delete_button'>삭제</button>
           </div>`).join('')}
@@ -35,6 +35,6 @@ function TodoResult() {
   updator.push(this.render);
 }
 
-const newTodoResult = () => new TodoResult().node;
+const newTodoResult = ({ category }) => new TodoResult({ category }).node;
 
 export default newTodoResult;
