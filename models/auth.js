41ffc89d8d9eb14e682b627a778c7e4ee8d25603
woomin <dal96k@gmail.com>
2020-09-20 13:41:23 +0900

    Modift connection to use pool
    기존의 그냥 커넥션을 맺던 방식에서 커넥션 풀을 이용하여 커넥션을 맺는
    방식으로 변경하였습니다. 커넥션 풀이 훨씬 장점이 많아서 이를
    선택했습니다.

--- models/auth.js
+++ models/auth.js
@@ -1,39 +1,51 @@
-const connection = require('./');
+const connectPool = require('./');
 
 const { checkError } = require('./util');
 
 const checkExistingUserById = (next) => (id) => new Promise((resolve, reject) => {
-  const query = `SELECT id from user where id=?`;
-  connection.query(query, [id], (error, rows, fields) => {
-    checkError(error, reject);
-    resolve(rows.length > 0);
+  connectPool(connection => {
+    const query = `SELECT id from user where id=?`;
+    connection.query(query, [id], (error, rows, fields) => {
+      checkError(error, reject);
+      resolve(rows.length > 0);
+    });
+    connection.release();
   });
 }).catch(error => next(error));
 
 const addNewUser = (next) => (id, password) => new Promise((resolve, reject) => {
-  const query = `INSERT INTO user (id, password) VALUES(?, ?)`;
-  connection.query(query, [id, password], (error, rows, fields) => {
-    checkError(error, reject);
-    resolve(true);
+  connectPool(connection => {
+    const query = `INSERT INTO user (id, password) VALUES(?, ?)`;
+    connection.query(query, [id, password], (error, rows, fields) => {
+      checkError(error, reject);
+      resolve(true);
+    });
+    connection.release();
   });
 }).catch(error => next(error));
 
 const checkPassword = (next) => (id, password) => new Promise((resolve, reject) => {
-  const query = `SELECT password from user where id=?`;
-  connection.query(query, [id], (error, rows, fields) => {
-    checkError(error, reject);
+  connectPool(connection => {
+    const query = `SELECT password from user where id=?`;
+    connection.query(query, [id], (error, rows, fields) => {
+      checkError(error, reject);
 
-    const [userInfo] = rows;
-    userInfo ? resolve(password === userInfo.password) : resolve(false);
+      const [userInfo] = rows;
+      userInfo ? resolve(password === userInfo.password) : resolve(false);
+    });
+    connection.release();
   });
 }).catch(error => next(error));
 
 const checkIsAuthorOfTodo = (next) => (userId, todoId) => new Promise((resolve, reject) => {
-  const query = `SELECT userid from todo where todoid=?`;
-  connection.query(query, [todoId], (error, rows, fields) => {
-    checkError(error, reject);
-    const [todo] = rows;
-    todo ? resolve(userId === todo.userid) : resolve(false);
+  connectPool(connection => {
+    const query = `SELECT userid from todo where todoid=?`;
+    connection.query(query, [todoId], (error, rows, fields) => {
+      checkError(error, reject);
+      const [todo] = rows;
+      todo ? resolve(userId === todo.userid) : resolve(false);
+    });
+    connection.release();
   });
 }).catch(error => next(error));
 
