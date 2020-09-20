8e531af2c21c3be43f56745d805d03db32f4b044
woomin <dal96k@gmail.com>
2020-09-20 12:58:36 +0900

    Modify normal query to prepared statements
    MySQL에서 쿼리문을 작성할 때 prepared statements를 사용하면 성능적인
    측면과, 보안적인 측면에서 이점을 얻을 수 있다는 호눅스님의 말씀을 듣고
    변경하였습니다.

--- models/auth.js
+++ models/auth.js
@@ -3,24 +3,24 @@ const connection = require('./');
 const { checkError } = require('./util');
 
 const checkExistingUserById = (next) => (id) => new Promise((resolve, reject) => {
-  const query = `SELECT id from user where id='${id}'`;
-  connection.query(query, (error, rows, fields) => {
+  const query = `SELECT id from user where id=?`;
+  connection.query(query, [id], (error, rows, fields) => {
     checkError(error, reject);
     resolve(rows.length > 0);
   });
 }).catch(error => next(error));
 
 const addNewUser = (next) => (id, password) => new Promise((resolve, reject) => {
-  const query = `INSERT INTO user (id, password) VALUES('${id}', '${password}')`;
-  connection.query(query, (error, rows, fields) => {
+  const query = `INSERT INTO user (id, password) VALUES(?, ?)`;
+  connection.query(query, [id, password], (error, rows, fields) => {
     checkError(error, reject);
     resolve(true);
   });
 }).catch(error => next(error));
 
 const checkPassword = (next) => (id, password) => new Promise((resolve, reject) => {
-  const query = `SELECT password from user where id='${id}'`;
-  connection.query(query, (error, rows, fields) => {
+  const query = `SELECT password from user where id=?`;
+  connection.query(query, [id], (error, rows, fields) => {
     checkError(error, reject);
 
     const [userInfo] = rows;
@@ -29,8 +29,8 @@ const checkPassword = (next) => (id, password) => new Promise((resolve, reject) 
 }).catch(error => next(error));
 
 const checkIsAuthorOfTodo = (next) => (userId, todoId) => new Promise((resolve, reject) => {
-  const query = `SELECT userid from todo where todoid='${todoId}'`;
-  connection.query(query, (error, rows, fields) => {
+  const query = `SELECT userid from todo where todoid=?`;
+  connection.query(query, [todoId], (error, rows, fields) => {
     checkError(error, reject);
     const [todo] = rows;
     todo ? resolve(userId === todo.userid) : resolve(false);
--- models/todo.js
+++ models/todo.js
@@ -11,32 +11,32 @@ const addTodo = (next) => (...elements) => new Promise((resolve, reject) => {
 }).catch(error => next(error));
 
 const getTodos = (next) => (userId) => new Promise((resolve, reject) => {
-  const query = `SELECT * from todo where userid='${userId}'`;
-  connection.query(query, (error, rows, fields) => {
+  const query = `SELECT * from todo where userid=?`;
+  connection.query(query, [userId], (error, rows, fields) => {
     checkError(error, reject);
     resolve(rows);
   });
 }).catch(error => next(error));
 
 const updateTodo = (next) => (todoId, updatedContent) => new Promise((resolve, reject) => {
-  const query = `UPDATE todo SET content='${updatedContent}' where todoid='${todoId}'`;
-  connection.query(query, (error, rows, fields) => {
+  const query = `UPDATE todo SET content=? where todoid=?`;
+  connection.query(query, [updatedContent, todoId], (error, rows, fields) => {
     checkError(error, reject);
     resolve(true);
   });
 }).catch(error => next(error));
 
 const deleteTodo = (next) => (todoId) => new Promise((resolve, reject) => {
-  const query = `DELETE from todo where todoid='${todoId}'`;
-  connection.query(query, (error, rows, fields) => {
+  const query = `DELETE from todo where todoid=?`;
+  connection.query(query, [todoId], (error, rows, fields) => {
     checkError(error, reject);
     resolve(true);
   });
 }).catch(error => next(error));
 
 const moveTodo = (next) => (todoId, category) => new Promise((resolve, reject) => {
-  const query = `UPDATE todo SET category='${category}' where todoid='${todoId}'`;
-  connection.query(query, (error, rows, fields) => {
+  const query = `UPDATE todo SET category=? where todoid=?`;
+  connection.query(query, [category, todoId], (error, rows, fields) => {
     checkError(error, reject);
     resolve(true);
   });
