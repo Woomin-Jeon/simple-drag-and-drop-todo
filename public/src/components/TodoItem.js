fd81bb64bf1e7e66e1b3b500fb78849596e12e96
woomin <dal96k@gmail.com>
2020-09-21 17:34:43 +0900

    Add todo edit modal to todo items
    투두 아이템들에 대해 모달창을 적용하였습니다.

--- public/src/components/TodoItem.js
+++ public/src/components/TodoItem.js
@@ -1,4 +1,5 @@
 import Button from './Button.js';
+import Modal from './Modal.js';
 
 function TodoItem({ todo, category }) {
   this.node = document.createElement('div');
@@ -6,17 +7,22 @@ function TodoItem({ todo, category }) {
   this.node.setAttribute('data-todoid', `${todo.todoid}`);
   this.node.setAttribute('draggable', 'true');
 
-  this.todoDragEvent = (event) => {
+  this.node.addEventListener('dblclick', () => {
+    const editModal = document.querySelector(`#todo_item_edit_modal_${todo.todoid}`);
+    const overlay = document.querySelector('#overlay');
+    
+    [editModal, overlay].forEach(dom => dom.classList.remove('hidden'));
+  });
+
+  this.render = () => {
+    this.node.innerHTML = `<span>${todo.content}</span>`;
+    this.node.appendChild(Button({ id: `todo_item_delete_button_${todo.todoid}`, className: 'todo_delete_button', title: '삭제' }))
+    this.node.appendChild(Modal({ id: `todo_item_edit_modal_${todo.todoid}`, title: 'Edit Note', text: todo.content }));
+    
     this.node.addEventListener('dragstart', (event) => {
       const targetTodoId = event.target.dataset.todoid;
       event.dataTransfer.setData('todoid', targetTodoId);
     });
-  }
-
-  this.render = () => {
-    this.node.innerHTML = `<span>${todo.content}</span>`;
-    this.node.appendChild(Button({ id: todo.todoid, className: 'todo_delete_button', title: '삭제' }))
-    this.todoDragEvent();
   };
 
   this.render();
