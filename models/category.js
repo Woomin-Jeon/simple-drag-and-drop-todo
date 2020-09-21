70e0ffdc1c6ace8e784ff1cc95690087ec5ced46
woomin <dal96k@gmail.com>
2020-09-21 23:23:38 +0900

    Add category model feature
    DB와 통신하여 카테고리를 변경해주는 쿼리를 작성하였습니다.

--- models/category.js
+++ models/category.js
@@ -3,7 +3,10 @@ const connectPool = require('./');
 const { checkError } = require('./util');
 
 const modifyCategoryName = (next) => (category, newCategory) => {
-  //Todo: Implement
+  connectPool(connection => {
+    const query = `UPDATE todo SET category=? where category=?`;
+    connection.query(query, [newCategory, category]);
+  });
 };
 
 module.exports = { modifyCategoryName };
