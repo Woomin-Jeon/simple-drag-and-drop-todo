fe46025754d4723fdacb05268cae05f1c17af339
woomin <dal96k@gmail.com>
2020-09-21 20:19:30 +0900

    Add category modifying api (not yet implemented)
    프론트에서 카테고리 명 변경을 요청하면 이를 받아서 처리하는 route를
    만들었습니다. 하지만 아직 DB 통신부분까지는 구현하지 못했습니다.

---
+++ models/category.js
@@ -0,0 +1,9 @@
+const connectPool = require('./');
+
+const { checkError } = require('./util');
+
+const modifyCategoryName = (next) => (category, newCategory) => {
+  //Todo: Implement
+};
+
+module.exports = { modifyCategoryName };
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.use('/auth', require('./auth'));
 router.use('/todo', require('./todo'));
+router.use('/category', require('./category'));
 
 module.exports = router;
