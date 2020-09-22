9dd387a083f42c367e925060014fa9d97efebddc
woomin <dal96k@gmail.com>
2020-09-22 14:46:05 +0900

    Add placeholder attribute
    Textarea에 placeholder 속성도 넣어주고 싶어서 추가하였습니다.

--- public/src/components/Textarea.js
+++ public/src/components/Textarea.js
@@ -1,9 +1,10 @@
-function Textarea({ id, text }) {
+function Textarea({ id, text, placeholder }) {
   this.node = document.createElement('textarea');
   this.node.innerText = text;
   this.node.setAttribute('id', id);
+  this.node.setAttribute('placeholder', placeholder);
 }
 
-const newTextarea = ({ id, text }) => new Textarea({ id, text }).node;
+const newTextarea = ({ id, text, placeholder }) => new Textarea({ id, text, placeholder }).node;
 
 export default newTextarea;
