8517e47d819fa31b9c9997c81fc3d63adb376678
woomin <dal96k@gmail.com>
2020-09-22 09:27:17 +0900

    Modify category title edit route and model
    카테고리 수정에 대한 라우트와 DB 통신 로직을 변경하였습니다. 기존에는
    그냥 Todo의 category column만 수정하는 것이었지만 이제는 유저에 따라
    카테고리를 관리하도록 변경하였습니다. 이 과정에서 쿼리문 여러개를
    사용해야했기 때문에 MySQL에서 지원하는 트랜잭션을 사용하였습니다.

--- routes/category.js
+++ routes/category.js
@@ -6,8 +6,9 @@ const model = require('../models/category');
 
 router.post('/', async (req, res, next) => {
   const { category, newCategory } = req.body;
+  const { userId } = req.session;
 
-  await model.modifyCategoryName(next)(category, newCategory);
+  await model.modifyCategoryName(next)(userId, category, newCategory);
   res.status(200).send('Complete to modify category name');
 });
 
