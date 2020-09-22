29936e25b0e27488b7ee62d250fffc255fa6e0a7
woomin <dal96k@gmail.com>
2020-09-22 19:59:02 +0900

    Apply css to header
    헤더에 CSS를 적용하였습니다.

--- public/src/components/Header.js
+++ public/src/components/Header.js
@@ -3,17 +3,17 @@ import Button from './Button.js';
 function Header() {
   this.node = document.createElement('header');
   this.node.classList.add('flex');
-
-  this.node.innerHTML = `<div>TODO</div>`;
+  this.node.classList.add('header');
 
   this.menuButtonEvent = () => {
     //TODO: 아래 코드 제거 및 이벤트 구현
     alert('menu button clicked');
   }
 
+  this.node.innerHTML = `<div>TODO</div>`;
   this.node.appendChild(Button({
     id: 'header_menu_button',
-    class: 'header_menu_button',
+    className: 'header_menu',
     title: 'menu',
     event: this.menuButtonEvent
   }));
