2bdfd73d17f5791f48e578c8fce51c094d16ea41
woomin <dal96k@gmail.com>
2020-09-23 15:53:30 +0900

    Create SideSlider component and apply it
    유저의 로그를 표시 할 사이드 슬라이더 컴포넌트를 추가하고 애니메이션
    기능을 추가하였습니다.

---
+++ public/src/components/SideSlider.js
@@ -0,0 +1,25 @@
+import Button from './Button.js';
+
+function SideSlider() {
+  this.node = document.createElement('div');
+  this.node.setAttribute('id', 'side_slider');
+  this.node.classList.add('slider');
+  this.node.classList.add('hidden');
+  
+  this.closeSliderButtonEvent = () => {
+    this.node.style.animationName = 'slider-close';
+    setTimeout(() => this.node.classList.add('hidden'), 1000);
+  };
+  
+  this.node.appendChild(Button({
+    id: 'side_slider_close_button',
+    className: 'side_slider_close_button',
+    title: 'x',
+    event: this.closeSliderButtonEvent,
+  }));
+  
+}
+
+const newSideSlider = () => new SideSlider().node;
+
+export default newSideSlider;
